12ac880008e6f90f2182e56e97202cc1e5f27163
serg-kas <serg_kas@mail.ru>
2023-07-30 23:49:23 +0300

    ready for docker - 3

--- app.py
+++ app.py
@@ -360,8 +360,8 @@ while True:
             if diff_time > 20:
                 start = time.time()
                 # Изображение в телегу
-                logger.info("Отправляем изображение в тлг старой функцией")
-                u.send_image_tlg(frame, bot_Token, chat_Id)
+                # logger.info("Отправляем изображение в тлг старой функцией")
+                # u.send_image_tlg(frame, bot_Token, chat_Id)
 
                 logger.info("Отправляем изображение в тлг")
                 image = cv.imencode('.jpg', frame)
